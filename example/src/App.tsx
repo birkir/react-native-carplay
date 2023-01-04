de968cae5788f4c2b6c85be93f4b12ab85286e98
Nick Melia <nicholasmelia@economist.com>
2023-01-04 01:23:56 +0000

    Removed use of navigation within the app and added queue

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,129 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Text, View } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { TabBar } from './screens/TabBar';
-import { NowPlaying } from './screens/NowPlaying';
-import { CarPlay } from 'react-native-carplay';
+import { CarPlay, InformationTemplate, ListTemplate, TabBarTemplate, AlertTemplate, NowPlayingTemplate } from 'react-native-carplay';
 import { Part } from './types/content';
+import fetchWeekly from './data/fetchWeekly';
+import SoundPlayer from 'react-native-sound-player';
+import queue from './data/queue';
 
 export type RootStackParamList = {
   TabBar: undefined;
   NowPlaying: { article: Part }
 }
 
-const Stack = createStackNavigator<RootStackParamList>();
+const DEC24_REF = "/content/f0e872f2ipoun91v1goki0jb4va2v5ei"
+const DEC17_REF = "/content/mkl0ncokb3kju08rpusfeptidohkvmcr"
+const DEC10_REF = "/content/c6btp3bpqls69m9ivokt8da56jdi2iti"
+
+const getTabBarTemplates = (articles, sections) => {
+  const homeTab = new InformationTemplate({
+    title: 'Home',
+    items: [],
+    actions: [],
+    onActionButtonPressed: () => {},
+    tabTitle: 'Home',
+    tabSystemImg: 'house'
+  })
+
+  const weeklyTab = new ListTemplate({
+    id: 'weekly',
+    sections: sections,
+    title: 'Weekly',
+    onItemSelect: async ({ index }) => {
+      onArticlePress(articles[index])
+      // NowPlaying(articles[index])
+    },
+    tabTitle: 'Weekly',
+    tabSystemImg: 'magazine'
+  });
+
+  const queueTab = new ListTemplate({
+    title: 'Queue',
+    sections: [
+      {
+        items: queue.getQueueItems().map((item) => ({
+          text: item.title
+        }))
+      }
+    ],
+    tabTitle: 'Queue',
+    tabSystemImg: 'list.triangle'
+  })
+
+  const searchTab = new InformationTemplate({
+    title: 'Search',
+    items: [],
+    actions: [],
+    onActionButtonPressed: () => {},
+    tabTitle: 'Search',
+    tabSystemImg: 'magnifyingglass'
+  })
+
+  return [homeTab, weeklyTab, queueTab, searchTab]
+}
+
+const onArticlePress = (article: Part) => {
+  const alertTemplate = new AlertTemplate({
+    titleVariants: [ article.title],
+    actions: [
+      {
+        id: 'play',
+        title: 'Play'
+      },
+      {
+        id: 'queue',
+        title: 'Queue'
+      },
+      {
+        id: 'cancel',
+        title: 'Cancel'
+      }
+    ],
+    onActionButtonPressed: ({ id }) => {
+      switch(id) {
+        case 'play': {
+          // if (article.audio?.main?.url?.canonical) {
+          //   SoundPlayer.playUrl(article.audio?.main?.url?.canonical)
+          // }
+
+          // CarPlay.dismissTemplate();
+          break;
+        }
+        case 'queue': {
+          queue.add({
+            id: article.tegID,
+            title: article.title,
+            url: article.audio?.main?.url?.canonical
+          })
+          CarPlay.dismissTemplate();
+          break;
+        }
+        case 'cancel': {
+          CarPlay.dismissTemplate();
+          break;
+        }
+        default:
+      }
+    }
+  });
+
+  CarPlay.presentTemplate(alertTemplate);
+}
+
+const TabBar = (ref: React.MutableRefObject<TabBarTemplate | undefined>) => {
+  const tabBarTemplate = new TabBarTemplate({
+    templates: [],
+    onTemplateSelect: () => {}
+  });
+
+  ref.current = tabBarTemplate
+
+  CarPlay.setRootTemplate(tabBarTemplate);
+}
 
 export const App = () => {
   const [carPlayConnected, setCarPlayConnected] = useState(CarPlay.connected);
+  const tabBarRef = useRef<TabBarTemplate | undefined>()
 
   useEffect(() => {
     function onConnect() {
@@ -36,13 +143,34 @@ export const App = () => {
     };
   });
 
+  useEffect(() => {
+    fetchWeekly(DEC24_REF).then(({ articles, sections }) => {
+      const templates = getTabBarTemplates(articles, sections)
+
+      tabBarRef.current?.updateTemplates({
+        templates,
+        onTemplateSelect: () => {}
+      });
+
+      queue.addListener(() => {
+        const templates = getTabBarTemplates(articles, sections)
+
+        tabBarRef.current?.updateTemplates({
+          templates,
+          onTemplateSelect: () => {}
+        });
+      })
+    })
+
+    TabBar(tabBarRef)
+
+    CarPlay.enableNowPlaying();
+  }, [])
+
   return carPlayConnected ? (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="TabBar">
-        <Stack.Screen name="TabBar" component={TabBar} />
-        <Stack.Screen name="NowPlaying" component={NowPlaying} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Car Play in progress</Text>
+    </View>
   ) : (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Please connect Car Play and open the test app</Text>
---
+++ example/src/data/queue.ts
@@ -0,0 +1,74 @@
+import SoundPlayer from 'react-native-sound-player'
+
+type AudioTrack = {
+  id: string
+  url: string
+  title: string
+}
+
+class AudioQueue {
+  private audioTracks: AudioTrack[]
+  private currentTrack: AudioTrack | undefined
+  private listeners: ((audioTracks: AudioTrack[]) => void)[]
+
+  constructor () {
+    this.audioTracks = []
+    this.listeners = []
+
+    SoundPlayer.addEventListener('FinishedPlaying', () => {
+      const nextAudioTrack = this.audioTracks.pop()
+
+      if (nextAudioTrack) {
+        SoundPlayer.playUrl(nextAudioTrack?.url)
+
+        this.next()
+      } else {
+        this.currentTrack = undefined
+      }
+    })
+  }
+
+  add (audioTrack: AudioTrack) {
+    this.audioTracks.push(audioTrack)
+
+    this.onQueueChange()
+  }
+
+  next () {
+    const nextAudioTrack = this.audioTracks.shift()
+
+    if (nextAudioTrack) {
+      SoundPlayer.playUrl(nextAudioTrack?.url)
+
+      this.currentTrack = nextAudioTrack
+
+      this.onQueueChange()
+    } else {
+      this.currentTrack = undefined
+    }
+  }
+
+  getCurrentTrack () {
+    return this.currentTrack
+  }
+
+  getQueueLength () {
+    return this.audioTracks.length
+  }
+
+  getQueueItems () {
+    return this.audioTracks
+  }
+
+  addListener (listener: (audioTracks: AudioTrack[]) => void) {
+    this.listeners.push(listener)
+  }
+
+  private onQueueChange () {
+    this.listeners.forEach((listener) => {
+      listener(this.audioTracks)
+    })
+  }
+}
+
+export default new AudioQueue()
