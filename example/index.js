8c2981ee37b2a2390b145e08a560090a68ffbd52
Luke Taylor <lmcjt@outlook.com>
2023-01-05 11:32:25 +0000

    update name

--- example/index.js
+++ example/index.js
@@ -1,11 +1,4 @@
-import React from 'react';
-import { View, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import { App } from './src/App';
 
-// function App() {
-//   return (
-//     <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
-//   )
-// }
-
-AppRegistry.registerComponent('test2', () => App);
+AppRegistry.registerComponent('ecodrive', () => App);
