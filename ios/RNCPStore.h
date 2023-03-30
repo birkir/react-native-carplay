eebeb78a36d58a588e3bb9f229b1a38621d451df
Benjamin Uzelac <ben@buzelac.com>
2023-03-30 10:38:30 -0500

    fix an issue with Objective-C++ ("template" is reserved in c++)

--- ios/RNCPStore.h
+++ ios/RNCPStore.h
@@ -11,7 +11,7 @@
 
 + (id)sharedManager;
 - (CPTemplate*) findTemplateById: (NSString*)templateId;
-- (NSString*) setTemplate:(NSString*)templateId template:(CPTemplate*)template;
+- (NSString*) setTemplate:(NSString*)templateId template:(CPTemplate*)carPlayTemplate;
 - (CPTrip*) findTripById: (NSString*)tripId;
 - (NSString*) setTrip:(NSString*)tripId trip:(CPTrip*)trip;
 - (CPNavigationSession*) findNavigationSessionById:(NSString*)navigationSessionId;
