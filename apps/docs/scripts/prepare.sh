9c7f3f14815d33e745bbe845ed642f283a902f40
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2023-11-13 10:51:47 +0000

    Update prepare.sh

--- apps/docs/scripts/prepare.sh
+++ apps/docs/scripts/prepare.sh
@@ -2,8 +2,10 @@
 
 scriptdir="$(dirname "$0")"
 
-# Copy README.md to pages/index.md for index page
+# Copy root markdown files
 cp "$scriptdir/../../../README.md" "$scriptdir/../pages/index.md"
+cp "$scriptdir/../../../CarPlay.md" "$scriptdir/../pages/CarPlay.md"
+cp "$scriptdir/../../../AndroidAuto.md" "$scriptdir/../pages/AndroidAuto.md"
 
 # Copy Static files
 cp -R "$scriptdir/../../../.github" "$scriptdir/../public/.github"
