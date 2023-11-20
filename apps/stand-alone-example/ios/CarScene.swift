b3432bc3f408e7104759fda48908411dc1095908
Daniel Kuhn <dkuhn@codevise.de>
2023-11-20 13:37:52 +0100

    Create bridge in stand-alone mode
    When launching the CarPlay scene directly on
    the CarPlay-client, create a bridge for the AppDelegate.
    Again check for an already existing bridge in order to
    not create multiple bridges:
    If the app is already running on the phone, a bridge will
    already be present on the AppDelegate.
    If not: create one for the CarPlay scene.

--- apps/stand-alone-example/ios/CarScene.swift
+++ apps/stand-alone-example/ios/CarScene.swift
@@ -4,6 +4,13 @@ import CarPlay
 class CarSceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
   func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
                                   didConnect interfaceController: CPInterfaceController) {
+    guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate) else { return }
+    
+    if appDelegate.bridge == nil {
+      appDelegate.bridge = RCTBridge(delegate: appDelegate, launchOptions: [:])
+      appDelegate.rootView = RCTAppSetupDefaultRootView(appDelegate.bridge, "RNCarPlayStandAlone", appDelegate.prepareInitialProps(), false)
+    }
+    
     RNCarPlay.connect(with: interfaceController, window: templateApplicationScene.carWindow);
   }
 
